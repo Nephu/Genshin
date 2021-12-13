7e97b2c09fb8b4488eab8dfd86c3ec6f30339f70
Nephu <70969963+Nephu@users.noreply.github.com>
2021-12-13 23:20:26 +0100

    Add files via upload

--- Jean.R
+++ Jean.R
@@ -27,7 +27,7 @@ Weapon <- tribble(~Atk_base, ~Atk_proc, ~Atk_flat, ~HP_base, ~HP_proc, ~HP_flat,
                   )
 
 Weapon_name <- tribble( ~Wpn, ~WName,
-                        0, "Primordial_Jade_Cutter",  # + buff 
+                        0, "Primordial_Jade_Cutter", 
                         1, "Aquila_Favonia",
                         2, "Skyward_Blade^on_buff", # + buff
                         3, "The_Flute", 
@@ -39,21 +39,21 @@ Sett <- Weapon + Jean
 Sett <- Sett %>% mutate( uchwyt = c("A_r") )
 
 Set_name <- tribble(~ASet, ~Name_Set,
-                    0, "Bez_setu",
+                    #0, "Bez_setu",
                     1, "2 Bloodstained + 2 Pale Flame",
                     2, "4 Gladiator", 
                     3, "2 Gladiator + 2 Shimenawa", 
                     4, "4 Viridescent",
-                    5, "")
+                    5, "4 Ocean-Hued Clam")
 
 ArtSet <- tribble(~Atk_AS, ~HP_AS, ~ELbuffAS, ~HealAS, ~PhysAS,
                   ~N_AS, ~CH_AS, ~E_AS, ~Q_AS, ~CrAS, ~CdAS, ~ASetAS, ~uchwyt,
-                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "A_r",
+                  #0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "A_r",
                   0, 0, 0, 0, 50, 0, 0, 0, 0, 0, 0, 1, "A_r",
                   18, 0, 0, 0, 0, 35, 0, 0, 0, 0, 0, 2, "A_r",
                   36, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, "A_r",
                   0, 0, 15, 0, 0, 0, 0, 0, 0, 0, 0, 4, "A_r",
-                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, "A_r",)
+                  0, 0, 0, 15, 0, 0, 0, 0, 0, 0, 0, 5, "A_r",)
 
 Sett2 <- left_join( Sett, ArtSet, "uchwyt") %>%
   mutate( Atk_proc = Atk_proc + Atk_AS, HP_proc = HP_proc + HP_AS,
@@ -63,9 +63,78 @@ Sett2 <- left_join( Sett, ArtSet, "uchwyt") %>%
           CritR = CritR + CrAS, CritD = CritD + CdAS, ASet = ASet + ASetAS) %>%
   select(1:18,)
 
-Staty <- left_join( Sett2, Artifact, "uchwyt") %>%
-  mutate( Atk_proc = Atk_proc + Art_AP,
-         ELbuff = ELbuff + El_dmg_G, Heal = Heal + HealAS, Phys = Phys + PhysAS,
-         N_bonus = N_bonus + N_AS, CH_bonus = CH_bonus + CH_AS, 
-         E_bonus = E_bonus + E_AS, Q_bonus = Q_bonus + Q_AS,
-         CritR = CritR + CrAS, CritD = CritD + CdAS, ASet = ASet + ASetAS)
+Statystyki <- left_join( Sett2, Artifact, "uchwyt") %>%
+  mutate( Atk_proc = Atk_proc + Art_AP, Atk_flat = Atk_flat + Art_AF, HP_flat = HP_flat + HP_flat_F,
+         ELbuff = ELbuff + El_dmg_G, Heal = Heal + Heal_C, Phys = Phys + Phys_G,
+         CritR = CritR + Art_CR, CritD = CritD + Art_CD) %>%
+  select( -HP_flat_F, -Phys_G, -El_dmg_G, -Heal_C, -Art_AP, -Art_AF, -Art_CR, -Art_CD)
+
+Statystyki2 <- left_join( Statystyki, Weapon_name) %>% left_join( Set_name) %>%
+  mutate( HP = HP_base*(1+HP_proc/100) + HP_flat,
+          Atk_flat = if_else( Wpn != 0, Atk_flat, Atk_flat + HP*1.2/100 ),
+          ATK = Atk_base*(1+Atk_proc/100) + Atk_flat ) %>%
+  mutate( NATK = ( ATK*118.32/100)*(1 + (N_bonus + Phys)/100 )*(1 + ( pmin(CritR,100) * CritD)/10000 ),
+          CATK = ( ATK*320.28/100)*(1 + (CH_bonus + Phys)/100 )*(1 + ( pmin(CritR,100) * CritD)/10000 ),
+          EATK = ( ATK*525.6/100)*(1 + (E_bonus + ELbuff)/100 )*(1 + ( pmin(CritR,100) * CritD)/10000 ),
+          QATK = ( ATK*764.64/100)*(1 + (Q_bonus + ELbuff)/100 )*(1 + ( pmin(CritR,100) * CritD)/10000 ),
+          Healing = ( ATK*452.16/100 + 3389)*(1 + Heal/100 ),
+          Regen = ( ATK*45.22/100 + 339)*(1 + Heal/100 )
+           )
+
+avgStaty <- Statystyki2 %>% group_by( WName, Wpn, Name_Set,
+                                      ASet, Art_type ) %>% summarise( Normal = mean(NATK),
+                                                                                 Charged = mean(CATK),
+                                                                                 Elemental = mean(EATK),
+                                                                                 Burst = mean(QATK), 
+                                                                                 Healing = mean(Healing), 
+                                                                                 Regen = mean(Regen) ) %>% ungroup()
+avgStaty2 <- avgStaty %>% gather("Rodzaj_ataku", "DMG", 6:11)
+
+ggplot( ) +
+  geom_col( data = filter(staty_pod_N, ASet == 1 & Rodzaj_ataku == "Burst"), aes( Art_type, DMG, fill = Rodzaj_ataku) ) +
+  geom_col( data = filter(staty_pod_N, ASet == 1 & Rodzaj_ataku == "Elemental"), aes( Art_type, DMG, fill = Rodzaj_ataku) ) +
+  geom_col( data = filter(staty_pod_N, ASet == 1 & Rodzaj_ataku == "Charged"), aes( Art_type, DMG, fill = Rodzaj_ataku) ) +
+  geom_col( data = filter(staty_pod_N, ASet == 1 & Rodzaj_ataku == "Normal"), aes( Art_type, DMG, fill = Rodzaj_ataku) ) +
+  geom_point( data = filter(staty_pod_N, ASet == 1 & Rodzaj_ataku == c("Healing","Regen") ),
+              aes( Art_type, DMG, color = Rodzaj_ataku) ) +
+  ylab("Przeciętne obrażenia i leczenie") + xlab("Kombinacje artefaktów: Sands, Goblet, Circlet") +
+  scale_fill_brewer( palette = "Set1") +
+  scale_color_brewer( palette = "Oranges") +
+  guides(color = guide_legend(title = "Leczenie"))+
+  ylab("Przeciętne obrażenia") + xlab("Kombinacje artefaktów: Sands, Goblet, Circlet") +
+  coord_flip() + facet_wrap(~WName)
+
+#ggplot( ) +
+#  geom_col( data = filter(avgStaty2, Rodzaj_ataku == "Burst"), aes( Art_type, DMG, fill = Rodzaj_ataku) ) +
+#  geom_col( data = filter(avgStaty2, Rodzaj_ataku == "Elemental"), aes( Art_type, DMG, fill = Rodzaj_ataku) ) +
+#  geom_col( data = filter(avgStaty2, Rodzaj_ataku == "Charged"), aes( Art_type, DMG, fill = Rodzaj_ataku) ) +
+#  geom_col( data = filter(avgStaty2, Rodzaj_ataku == "Normal"), aes( Art_type, DMG, fill = Rodzaj_ataku) ) +
+#  geom_point( data = filter(avgStaty2, Rodzaj_ataku == "Healing"), aes( Art_type, DMG, color = Rodzaj_ataku) ) +
+#  geom_point( data = filter(avgStaty2, Rodzaj_ataku == "Regen"), aes( Art_type, DMG, color = Rodzaj_ataku) ) +
+#  ylab("Przeciętne obrażenia i leczenie") + xlab("Kombinacje artefaktów: Sands, Goblet, Circlet") +
+#  scale_fill_viridis_d( option = "C") +
+#  scale_color_brewer() +
+#  theme_bw() +
+#  guides(color = guide_legend(title = "Leczenie"))+
+#  coord_flip() + facet_grid(Name_Set~WName)
+
+staty_pod_N <- Statystyki2 %>% arrange( desc(NATK) ) %>% distinct( Art_type, Wpn, ASet, .keep_all = TRUE )
+colnames(staty_pod_N)[25:28] = c("Normal", "Charged", "Elemental", "Burst")
+staty_pod_N <- staty_pod_N %>% gather("Rodzaj_ataku", "DMG", 25:30)
+  
+staty_pod_C <- Statystyki2 %>% arrange( desc(CATK) ) %>% distinct( Art_type, Wpn, ASet, .keep_all = TRUE )
+colnames(staty_pod_C)[25:28] = c("Normal", "Charged", "Elemental", "Burst")
+staty_pod_C <- staty_pod_C %>% gather("Rodzaj_ataku", "DMG", 25:30)
+
+staty_pod_E <- Statystyki2 %>% arrange( desc(EATK) ) %>% distinct( Art_type, Wpn, ASet, .keep_all = TRUE )
+colnames(staty_pod_E)[25:28] = c("Normal", "Charged", "Elemental", "Burst")
+staty_pod_E <- staty_pod_E %>% gather("Rodzaj_ataku", "DMG", 25:30)
+
+staty_pod_Q <- Statystyki2 %>% arrange( desc(QATK) ) %>% distinct( Art_type, Wpn, ASet, .keep_all = TRUE )
+colnames(staty_pod_Q)[25:28] = c("Normal", "Charged", "Elemental", "Burst")
+staty_pod_Q <- staty_pod_Q %>% gather("Rodzaj_ataku", "DMG", 25:30)
+
+staty_pod_H <- Statystyki2 %>% arrange( desc(Healing) ) %>% distinct( Art_type, Wpn, ASet, .keep_all = TRUE )
+colnames(staty_pod_H)[25:28] = c("Normal", "Charged", "Elemental", "Burst")
+staty_pod_H <- staty_pod_H %>% gather("Rodzaj_ataku", "DMG", 25:30)
+  
--- std_artifact.R
+++ std_artifact.R
@@ -14,11 +14,11 @@ Goblet <- tribble(~Atk_proc_G, ~Phys_G, ~El_dmg_G, ~G_typ, ~Waga_G, ~uchwyt,
                   0, 58.3, 0, "Phys", 0, "A_r",
                   0, 0, 46.6, "EL", 4, "A_r")
 
-Circlet <- tribble(~Atk_proc_C, ~CR_C, ~CD_C, ~Heal, ~C_typ, ~Waga_C, ~uchwyt,
+Circlet <- tribble(~Atk_proc_C, ~CR_C, ~CD_C, ~Heal_C, ~C_typ, ~Waga_C, ~uchwyt,
                    46.6, 0, 0, 0, "Atk", 1, "A_r",
                    0, 31.1, 0, 0, "CR", 7, "A_r",
                    0, 0, 62.2, 0, "CD", 13, "A_r",
-                   0, 0, 0, 35.9, "Heal", 17, "A_r")
+                   0, 0, 0, 35.9, "Heal", 19, "A_r")
 
 avgaf <- tribble(~sub_af, ~waf, ~ile, ~uchwyt,
                    "a", 0, 0, "A_r",
@@ -31,14 +31,14 @@ avgap <- tribble(~sub_ap, ~wap, ~ile, ~uchwyt,
                    "A", 29.73, 6, "A_r")
 
 avgcr <- tribble(~sub_cr, ~wcr, ~ile, ~uchwyt,
-                 "R", 0, 0, "A_r",
-                 "R", 3.305, 1, "A_r",
-                 "R", 19.83, 6, "A_r")
+                 "C", 0, 0, "A_r",
+                 "C", 3.305, 1, "A_r",
+                 "C", 19.83, 6, "A_r")
 
 avgcd <- tribble(~sub_cd, ~wcd, ~ile, ~uchwyt,
-                 "D", 0, 0, "A_r",
-                 "D", 6.605, 1, "A_r",
-                 "D", 39.63, 6, "A_r")
+                 "X", 0, 0, "A_r",
+                 "X", 6.605, 1, "A_r",
+                 "X", 39.63, 6, "A_r")
 
 avg_sub_stat <- left_join( avgap, avgaf, "uchwyt") %>%
   left_join( avgcr, "uchwyt") %>%
@@ -62,6 +62,18 @@ FFlower <- left_join(Flower, avg_sub_stat1, "uchwyt") %>%
 PPlume <- left_join(Plume, avg_sub_stat1, "uchwyt") %>% 
   filter( ile_razem == 9) %>%
   select( -ile_razem)
+
+ile_subart <- function( wejscie ){
+  a_ilosc = str_count( wejscie, "a")
+  A_ilosc = str_count( wejscie, "A")
+  C_ilosc = str_count( wejscie, "C")
+  X_ilosc = str_count( wejscie, "X")
+  a_war = if_else( a_ilosc == 0, "", if_else( a_ilosc == 1, "a ", paste( a_ilosc, "a ", sep = "" ) )  )
+  A_war = if_else( A_ilosc == 0, "", if_else( A_ilosc == 1, "A ", paste( A_ilosc, "A ", sep = "" ) )  )
+  C_war = if_else( C_ilosc == 0, "", if_else( C_ilosc == 1, "C ", paste( C_ilosc, "C ", sep = "" ) )  )
+  X_war = if_else( X_ilosc == 0, "", if_else( X_ilosc == 1, "X ", paste( X_ilosc, "X ", sep = "" ) )  )
+  wyjscie = paste( a_war, A_war, C_war, X_war, sep = "")
+}
 
 Art_FP <- left_join(FFlower, PPlume, "uchwyt") %>%
   mutate( Art_sub_fp = paste(Art_sub_type.x, Art_sub_type.y, sep = "") ) %>%
@@ -91,9 +103,9 @@ ARt_GSFP <- left_join( ARt_SFP, GGoblet, "uchwyt") %>%
 
 CCirclet <- left_join(Circlet, avg_sub_stat1, "uchwyt") %>%
   filter( !(Waga_C == 1 & wap > 0), !(Waga_C == 7 & wcr > 0), !(Waga_C == 13 & wcd > 0) ) %>%
-  filter( !(Waga_C == 17 & ile_razem == 8) ) %>%
+  filter( !(Waga_C == 19 & ile_razem == 8) ) %>%
   select( -ile_razem, -Waga_C)
-############################ ?????????????????????????????
+
 Artifact_X <- left_join( ARt_GSFP, CCirclet, "uchwyt")
 
 Artifact <- Artifact_X %>%
@@ -108,4 +120,6 @@ Artifact <- Artifact_X %>%
   mutate( Art_AF = Atk_flat_P + waf) %>%
   select( -Atk_flat_P, -waf) %>%
   mutate( Art_CR = CR_C + wcr, Art_CD = CD_C + wcd) %>%
-  select( -CR_C, -wcr, -CD_C, -wcd)
+  select( -CR_C, -wcr, -CD_C, -wcd) %>%
+  mutate( Art_sub_x = trimws(ile_subart(Art_sub_x)) ) %>%
+  distinct(Art_type, Art_sub_x, .keep_all = TRUE)
